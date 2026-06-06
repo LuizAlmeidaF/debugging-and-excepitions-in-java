97efd8bc2a3155cca9acfe8fd04d3a8ff35a4333
luizalmeidaf <luiz.p.fraga10@gmail.com>
2026-06-06 11:59:19 -0300

    Adicionando mais codigo

--- src/br/com/dio/dao/UserDAO.java
+++ src/br/com/dio/dao/UserDAO.java
@@ -1,4 +1,12 @@
 package br.com.dio.dao;
+import br.com.dio.model.UserModel;
+import java.util.ArrayList;
 
 public class UserDAO {
+
+    private List<UserModel> models = new ArrayList<>;
+
+    public UserModel save(final UserModel models){
+
+    }
 }
--- src/br/com/dio/model/UserModel.java
+++ src/br/com/dio/model/UserModel.java
@@ -1,4 +1,64 @@
 package br.com.dio.model;
 
-public record UserModel() {
+import java.time.OffsetDateTime;
+import java.util.Objects;
+
+// tempo do video 6:09
+
+public class UserModel {
+        private long id;
+        private String name;
+        private String email;
+        private OffsetDateTime birthday;
+
+
+    public UserModel() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public OffsetDateTime getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(OffsetDateTime birthday) {
+        this.birthday = birthday;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        UserModel userModel = (UserModel) o;
+        return id == userModel.id &&
+                Objects.equals(name, userModel.name) &&
+                Objects.equals(email, userModel.email) &&
+                Objects.equals(birthday, userModel.birthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, email, birthday);
+    }
 }
